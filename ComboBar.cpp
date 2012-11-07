15639a10e8dc857f401fa9fd0378dce16b358bbc
pzi1win2go3 <paranoidmonkeyzhao@gmail.com>
2012-11-08 00:42:13 +0800

    Class ComboBar basically done

---
+++ ComboBar.cpp
@@ -0,0 +1,52 @@
+#include 'ComboBar.h'
+
+ComboBar::ComboBar()
+{
+	maxValue = MAXVALUE;
+	highestValue = 1;
+	value = 1;
+}
+
+ComboBar::init()
+{
+	maxValue = MAXVALUE;
+	highestValue = 1;
+	value = 1;
+}
+
+ComboBar::getValue()
+{
+	return value;
+}
+
+ComboBar::getMaxValue()
+{
+	return maxValue;
+}
+
+ComboBar::getHighestValue()
+{
+	return highestValue;
+}
+
+ComboBar::fallDown(double down)
+{
+	if (value - down >= 1)
+		value -= down;
+	
+	else
+		value = 1;
+}
+
+ComboBar::bingo()
+{
+	score += value;
+
+	if (value + INCREASE <= maxValue)
+		value += INCREASE;
+	else
+		value = maxValue;
+
+	if (value > highestValue)
+		highestValue = value;
+}
